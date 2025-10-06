5655d2617490fc1af8a08da2990e8814e95fa21c
igna2030 <imalaguti05@gmail.com>
2025-10-06 12:27:38 -0300

    Update app.js

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -5,6 +5,9 @@ import http from 'http';
 import path from 'path';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
+import viewsRouter from './routes/views.router.js';
+import ProductManager from './managers/ProductManager.js';
+import { Socket } from 'dgram';
 
 const app = express();
 const port = 8080;
@@ -23,12 +26,46 @@ app.set('views',path.resolve('./src/views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.static(path.resolve('./src/public')));
 
 // Asignación de routers
 app.use('/api/productos', productsRouter);
 app.use('/api/carts', cartsRouter);
+app.use('/', viewsRouter);
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+const productManager = new ProductManager('./src/data/productos.json')
+
+io.on('connection', async (socket)=>{
+    console.log('Cliente conectado');
+    const initialProducts =  await productManager.getProducts();
+    socket.emit('productos', initialProducts);
+    
+    socket.on('new-product',async (productData)=>{
+        try {
+            await productManager.addProduct(productData);
+            const updatedProducts = await productManager.getProducts();
+            io.sockets.emit('productos', updatedProducts);
+            
+        } catch (error) {
+            console.log("Error con el producto",error.message);
+            socket.emit('prouct-error',{message: error.message});
+        }
+    });
+
+    socket.on('deleteProduct',async (id)=>{
+        try {
+            await productManager.delete_product(id);
+            const updatedProducts = await productManager.getProducts();
+            io.sockets.emit('productos', updatedProducts);
+            
+        } catch (error) {
+            console.log("Error al eliminar el producto",error.message);
+            socket.emit('prouct-error',{message: error.message});
+            
+        }
+    });
+})
+
+httpServer.listen(port, () => {
+    console.log(` Servidor Express y Socket.io escuchando en el puerto ${port}`);
 });
