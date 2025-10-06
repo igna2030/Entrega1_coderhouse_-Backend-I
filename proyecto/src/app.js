fa1c4c631f43f7d61b91e833d47044b9544f3b92
igna2030 <imalaguti05@gmail.com>
2025-10-06 11:32:12 -0300

    Agregando las handlebars

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -1,10 +1,25 @@
 import express from 'express';
-import cors from 'cors';
+import { engine } from 'express-handlebars';
+import { Server as IOServer } from 'socket.io'; 
+import http from 'http';
+import path from 'path';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 
 const app = express();
 const port = 8080;
+
+const httpServer = http.createServer(app);
+const io = new IOServer(httpServer);
+
+app.use((req,res,next)=>{
+    req.io = io
+    next()
+});
+
+app.engine('handlebars',engine());
+app.set('view engine','handlebars');
+app.set('views',path.resolve('./src/views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
---
+++ proyecto/src/routes/views.router.js
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import ProductManager from '../managers/ProductManager.js';
+
+const views_router = Router();
+const product_manager = new ProductManager('src/json/productos.json');
+
+views_router.get("/home",async (req,res)=>{
+    try {
+        var productos = await product_manager.getProducts();
+        res.render("home",{productos:producto})
+        
+    } catch (error) {
+        res.status(500).json({error:"Error inesperado."});
+        
+    }
+});
+
+views_router.get("/realtimeproducts",async (req,res)=>{
+    try {
+        var productos = await product_manager.getProducts();
+        res.render("home",{productos:producto})
+        
+    } catch (error) {
+        res.status(500).json({error:"Error inesperado."});
+        
+    }
+});
+
+export default views_router;
